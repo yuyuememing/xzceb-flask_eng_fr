19ea7882221b120c5ac2ad5b950c1dd3ad721470
yuyuememing <95630242+yuyuememing@users.noreply.github.com>
2021-12-11 02:07:46 +0800

    Update translator.py

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,9 +1,9 @@
-#import json
+import json
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
-#apikey = os.environ['apikey']
-#url = os.environ['url']
+apikey = os.environ['apikey']
+url = os.environ['url']
 
 authenticator = IAMAuthenticator('apikey')
 language_translator = LanguageTranslatorV3(
