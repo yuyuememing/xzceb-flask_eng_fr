33dad31c5c918272462ede0aba4d52daeafe49bc
yuyuememing <yuyuememing@gmail.com>
2021-12-10 17:59:13 +0000

    Final changes

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -5,8 +5,6 @@ from translator import french_to_english
 class TestTranslator(unittest.TestCase):
     def null_english_to_french(self):
         self.assertNotEqual(english_to_french('None'),'')
-    def null_english_to_french(self):
-        self.assertNotEqual(english_to_french('0'),'0')
     def test_english_to_french(self):
         self.assertEqual(english_to_french('Hello'),'Bonjour')
     def test_english_to_french(self):
@@ -15,8 +13,6 @@ class TestTranslator(unittest.TestCase):
 class TestTranslator1(unittest.TestCase):
     def null_french_to_english(self):
         self.assertNotEqual(french_to_english('None'),'')
-    def null_french_to_english(self):
-        self.assertNotEqual(french_to_english('0'),'0')
     def test_french_to_english(self):
         self.assertEqual(french_to_english('Bonjour'),'Hello')
     def test_french_to_english(self):
